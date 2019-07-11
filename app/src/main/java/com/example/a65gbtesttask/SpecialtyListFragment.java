9d9a46ed91a43d5978fc4effef5af58a64b9f9c8
AntonKop <antohapp@gmail.com>
2019-07-11 15:50:38 +0400

    add recyclerview

--- app/src/main/java/com/example/a65gbtesttask/SpecialtyListFragment.java
+++ app/src/main/java/com/example/a65gbtesttask/SpecialtyListFragment.java
@@ -8,15 +8,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class SpecialtyListFragment extends Fragment {
 
+    private RecyclerView specialtysRecyclerView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.specialty_list_fragment_layout, container, false);
+        specialtysRecyclerView = view.findViewById(R.id.specialty_list);
+        specialtysRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return view;
     }
 }
